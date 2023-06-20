1ea4c5f3115d6b9740ce5dbb0cfed20df8c86ca6
rorense <rkorense@gmail.com>
2023-06-20 21:05:26 +1200

    Adding necessary files

---
+++ jamming/src/Components/App/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import PlayList from '../PlayList/PlayList';
+
+export default function App() {
+    return (
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <SearchBar />
+        <div className="App-playlist">
+          <SearchResults /> 
+          <PlayList />
+        </div>
+      </div>
+    </div>
+  );
+}

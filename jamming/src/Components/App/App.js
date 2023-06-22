76b7677194417a5683754d4b7bc23600ef69879f
rorense <rkorense@gmail.com>
2023-06-22 19:47:08 +1200

    Adding functionality of search bar

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -41,6 +41,8 @@ export class App extends React.Component {
   this.addTrack = this.addTrack.bind(this);
   this.removeTrack = this.removeTrack.bind(this);
   this.updatePlaylistName = this.updatePlaylistName(this);
+  this.savePlaylist = this.savePlaylist(this);
+  this.search = this.search(this);
   }; 
 
   addTrack(track) {
@@ -64,20 +66,29 @@ export class App extends React.Component {
   updatePlaylistName(name) {
     this.setState({ playlistName: name })
   }
+
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map(track => track.uri)
+  }
+
+  search (term) {
+    console.log(term);
+  }
     
   render() {
     return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-        <SearchBar />
+        <SearchBar onSearch={this.search} />
         <div className="App-playlist">
           <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/> 
           <PlayList 
           playlistName={this.state.playlistName} 
           playlistTracks={this.state.playlistTracks} 
           onRemove={this.removeTrack}
-          onNameChange={this.updatePlaylistName}/>
+          onNameChange={this.updatePlaylistName}
+          onSave={this.savePlaylist}/>
         </div>
       </div>
     </div>

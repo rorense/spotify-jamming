3ffb7c809b70ee19c923d9d962034f8e5dd8c004
rorense <rkorense@gmail.com>
2023-06-27 21:29:54 +1200

    Working on search with spotify API

--- jamming/src/util/spotify.js
+++ jamming/src/util/spotify.js
@@ -1,13 +1,42 @@
-const accessToken = "";
+let accessToken = "";
+const clientID = "0d8fe520136844b4aaf6fa9e4ebf8877";
+const redirectURI = "http://localhost:3000/";
 const Spotify = {
+
     getAccessToken() {
         if (accessToken) {
             return accessToken;
         }
-        window.location.href.match(/access_token=([^&]*)/);
-        window.location.href.match(/expires_in=([^&]*)/);
+        const urlAccessToken = window.location.href.match(/access_token=([^&]*)/);
+        const urlExpiresIn = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (urlAccessToken && urlExpiresIn) {
+            accessToken = urlAccessToken[1];
+            const expires_in = Number(urlExpiresIn[1]);
+            window.setTimeout(() => accessToken = '', expires_in * 1000);
+            window.history.pushState('Access Token', null, '/');
+
+        } else {
+            const redirect = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+            window.location = redirect;
+        }
+    },
+
+    search(term) {
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+            headers: { Authorization: `Bearer ${accessToken}`}
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((jsonResponse) => {
+            if(!jsonResponse.tracks) {
+                return [];
+            }
+            return jsonResponse.tracks.items
+        }) 
     }
 
 }
-
 export {Spotify}
